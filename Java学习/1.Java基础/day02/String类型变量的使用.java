6f383308c435c9cdf0510553d147f29efcee47f4
mengkai ke <282913448@qq.com>
2021-04-02 15:15:15 +0800

    String类型学习1

--- Java学习/1.Java基础/day02/String类型变量的使用.java
+++ Java学习/1.Java基础/day02/String类型变量的使用.java
@@ -18,10 +18,30 @@ class VariableTest {
 
 				//*********************************/
 				int number = 1001;
-				String numberStr = "学好：";
+				String numberStr = "学号：";
 				String info = numberStr + number; // +: 连接运算
 				boolean b1 = true;
 				String info1 = info + b1;// +: 连接运算
 				System.out.println(info);
+
+				//*********************************/
+				// 练习1
+				char c = 'a'; // a: 97 A: 65
+				int num = 10;
+				String str= "hello";
+				System.out.println(c + num + str); // 107hello
+				System.out.println(c + str + num); // ahello10
+				System.out.println(c + (num + str)); // a10hello
+				System.out.println((c + num) + str); // 107hello
+				System.out.println(str + num + c); // hello10a
+
+				System.out.println("*********************************")
+				// 练习2
+				// * *
+				// System.out.println("*	*");
+				System.out.println("*" + "\t" + "*");
+				System.out.println('*' + "\t" + '*');
+				System.out.println('*' + '\t' + "*");
+				System.out.println('*' + ('\t' + "*"));
 		}
 }
