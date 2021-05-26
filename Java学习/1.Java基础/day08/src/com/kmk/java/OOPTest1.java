1b5f4cf025321b727306f4d5a36d7631f7f8b232
ke <282913448@qq.com>
2021-05-26 21:46:23 +0800

    增加新的笔记

--- Java学习/1.Java基础/day08/src/com/kmk/java/OOPTest1.java
+++ Java学习/1.Java基础/day08/src/com/kmk/java/OOPTest1.java
@@ -45,7 +45,8 @@ package com.kmk.java;
  * 三、面向对象的两个要素：
  * 类：对一类事物的描述，是抽象的、概念上的定义
  * 对象：是实际存在的该类事物的每个个体，因而也称为实例(instance)
- *
+ * >面向对象程序设计的重点是类的设计
+ * >设计类，就是设计类的成员
  **/
 public class OOPTest1 {
     public static void main(String[] args) {
